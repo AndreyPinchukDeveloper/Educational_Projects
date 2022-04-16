15ddc7f338aac07f7b533c81e90874746d61cfce
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-04-16 12:56:50 +0700

    Development update #1
    - WPF app for cheking your pesonal tasks.

---
+++ MyTasks/MyTasks/Models/TaskModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTasks.Models
+{
+    class TaskModel
+    {
+        private bool _isDone;
+        private string _text;
+
+        public DateTime CreationData { get; set; } = DateTime.Now;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+
+    }
+}
