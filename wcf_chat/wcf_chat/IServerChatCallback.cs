baef7f8b682f6c386972b11d2d26194eb45094dc
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-04-24 17:37:35 +0700

    Development Update #8
    - client-server chat(WCF);

---
+++ wcf_chat/wcf_chat/IServerChatCallback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace wcf_chat
+{
+    public interface IServerChatCallback
+    {
+        [OperationContract(IsOneWay = true)]//isoneway means that the server won't wait answer, do their own job immedeately
+        void MessageCallback(string message, int id);
+    }
+}
---
+++ wcf_chat/wcf_chat/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Service : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser userToAdd = new ServerUser() 
+            {
+                ID = nextId, 
+                Name = name, 
+                operationContext = OperationContext.Current 
+            };
+
+            nextId++;
+            SendMessage(userToAdd.Name + "was connected.", userToAdd.ID);
+            users.Add(userToAdd);//adding new user in list
+            return userToAdd.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var userToRemove = users.FirstOrDefault(i => i.ID == id);
+            if (userToRemove != null)
+            {
+                users.Remove(userToRemove);
+                SendMessage(userToRemove.Name + "has left.", userToRemove.ID);
+            }
+        }
+
+        public void SendMessage(string message, int id)
+        {
+            
+        }
+    }
+}
