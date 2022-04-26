4435adcafd4b720eed8a87914317891eeef17acc
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-04-26 08:38:00 +0700

    Development Update #10
    - WPF window for chat.

--- wcf_chat/Chat_client/MainWindow.xaml.cs
+++ wcf_chat/Chat_client/MainWindow.xaml.cs
@@ -12,17 +12,65 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Chat_client.ServiceChat;
 
 namespace Chat_client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IServiceChatCallback
     {
+        bool isConnected = false;
+        ServiceChatClient client;
+        int ID;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+        }
+
+        void ConnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(ID);
+                UserName.IsEnabled = true;
+                ConnectButton.Content = "Connect";
+                isConnected = false;
+            }
+        }
+
+        void DisconnectUser()
+        {
+            if (!isConnected)
+            {
+                ID = client.Connect(UserName.Text);
+                UserName.IsEnabled = false;
+                DisconnectButton.Content = "Disconnect";
+                isConnected = true;
+            }
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectUser();
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        public void MessageCallback(string message)
+        {
+            listBoxChat.Items.Add(message);
         }
     }
 }
