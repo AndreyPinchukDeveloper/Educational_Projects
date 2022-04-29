987ad0bf874a68dd85389ce1d19e9056ee010a92
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-04-29 18:28:49 +0700

    Development Update #13
    - ScreenSaver.

---
+++ ScreenSaver/ScreenSaver/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class Form1 : Form
+    {
+        private Star[] stars = new Star[1500];
+        private Random random = new Random();
+        private Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new Star()
+                {
+                    X = random.Next(-pictureBox1.Width, pictureBox1.Width),
+                    Y = random.Next(-pictureBox1.Height, pictureBox1.Height),
+                    Z = random.Next(1, pictureBox1.Width)
+                };
+            }
+
+            timer1.Start();
+        }
+    }
+}
---
+++ ScreenSaver/ScreenSaver/Star.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSaver
+{
+    public class Star
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        private float Map(float n, float start1, float stop1, float start2, float stop2)
+        {
+            return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
