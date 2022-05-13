ca538b2d8341c478754200d2d558a1400ce8a279
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-05-13 15:58:33 +0700

    Development Update 24
    - console apps fpr tests.

---
+++ Route256Tasks/Route256Tasks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Route256Tasks
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int firstValue = 0;
+            int secondValue = 0;
+            while (true)
+            {
+                int value = Convert.ToInt32(Console.ReadLine());
+                firstValue = value;
+                int second = Convert.ToInt32(Console.ReadLine());
+                secondValue = value;
+                SumOfTwoNumbers(firstValue,secondValue);
+            }
+        }
+
+        private static int SumOfTwoNumbers(int first, int second)
+        {
+            int sum = 0 ;
+            sum = first + second;
+            Console.WriteLine(sum);
+            return sum;
+        }
+
+        
+    } 
+}
---
+++ Route256Tasks/Route256Tasks/Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route256Tasks
+{
+    public class Kata
+    {
+        public static int[][] Pyramid(int n)
+        {
+            int[][] array = new int[n][];
+            int valueToCountArray = n - 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = new int[i + 1];
+            }
+
+            if (n == 0)
+            {
+                return array;
+            }
+            else if (n == 1)
+            {
+                array[valueToCountArray].SetValue(1, 0);
+            }
+
+            else if (n > 0)
+            {
+                while (valueToCountArray >= 0)
+                {
+                    for (int i = 0; i <= valueToCountArray; i++)
+                    {
+                        array[valueToCountArray].SetValue(1, i);
+                    }
+                    valueToCountArray--;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                System.Console.Write("Element({0}): ", i);
+
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", array[i][j], j == (array[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+            Console.WriteLine(array.Length);
+            return array;
+        }
+    }
+}
