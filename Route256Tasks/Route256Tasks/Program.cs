82e86b7b5e57ecb8ac19368700c8a7640b8afbc8
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-05-17 23:20:54 +0700

    Development Update # 29
    - new folders in the middle, has started next UI (view all).

--- Route256Tasks/Route256Tasks/Program.cs
+++ Route256Tasks/Route256Tasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Route256Tasks
@@ -8,7 +9,23 @@ namespace Route256Tasks
         
         static void Main(string[] args)
         {
+            int[] myArray = new int[] { 1,2,3,4,5};
+            BinarySearch(myArray, 4);
 
+        }
+        private static bool BinarySearch(int[] array, int valueToFind)
+        {
+            bool content = true;
+            int lowerValue = 0;
+            int higherValue = array.Length - 1;
+            int middle = 0;
+
+            while (lowerValue<=higherValue)
+            {
+
+            }
+
+            return content;
         }
     } 
 }
