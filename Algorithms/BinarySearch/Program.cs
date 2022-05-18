7cfdd1aca3b074b209f6ef40934c7164f5a01a0f
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-05-18 21:20:35 +0700

    Development Updatre # 32
    - binarySearch for c# has implemented.

--- Algorithms/BinarySearch/Program.cs
+++ Algorithms/BinarySearch/Program.cs
@@ -10,21 +10,35 @@ namespace BinarySearch
         static void Main(string[] args)
         {
             int[] myArray = new int[] { 1,2,3,4,5};
-            BinarySearch(myArray, 4);
+            BinarySearch(myArray, 6);
+            Console.ReadLine();
 
         }
         private static bool BinarySearch(int[] array, int valueToFind)
         {
-            bool content = true;
+            bool content = false;
             int lowerValue = 0;
             int higherValue = array.Length - 1;
-            int middle = 0;
 
             while (lowerValue<=higherValue)
             {
-
+                int middle = lowerValue + higherValue;//4
+                int value = array[middle];
+                if (value == valueToFind)
+                {
+                    Console.WriteLine("true");
+                    return content = true;
+                }
+                else if (value>valueToFind)
+                {
+                    higherValue = middle - 1;
+                }
+                else if (value<valueToFind)
+                {
+                    lowerValue = middle + 1;
+                }
             }
-
+            Console.WriteLine("false");
             return content;
         }
     } 
