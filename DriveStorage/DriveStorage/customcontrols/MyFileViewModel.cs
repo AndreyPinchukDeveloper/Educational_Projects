301db7d96dedd759ca9cee6290e74f05e32e706f
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-05-18 16:55:19 +0700

    Development Update #31
    - a CustomList creation has started.

---
+++ DriveStorage/DriveStorage/customcontrols/MyFileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DriveStorage.customcontrols
+{
+    class MyFileViewModel
+    {
+        //Resource dictionary
+        ResourceDictionary dict = Application.LoadComponent(new Uri
+            ("DriveStorage;component/icons.xaml", UriKind.RelativeOrAbsolute)) 
+            as ResourceDictionary;
+
+        //public ObservableCollection
+    }
+
+    class GetFileDetails
+    {
+        public PathGeometry FileThumb { get; set; }
+        public string Fill { get; set; }
+    }
+}
