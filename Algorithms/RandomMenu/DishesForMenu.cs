54e39aaa758b30adbc3d56703b7884245030d7b4
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-05-21 20:18:02 +0700

    HotFix
    - work with lists of objects.

---
+++ Algorithms/RandomMenu/DishesForMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomMenu
+{
+    class DishesForMenu
+    {
+        public string[] dishes = new string[10]
+        {
+            "Braised Leeks with Mozzarella",
+            "Osteria Stellina",
+            "Smoked Pork Jowl with Pickles",
+            "Scallop Sashimi with Meyer Lemon Confit",
+            "Vegan Charcuterie",
+            "Pappardelle with Sea Urchin and Cauliflower",
+            "Pork Rillette Hand Pies",
+            "Malted Custard French Toast",
+            "Pasta with Lamb Ragù",
+            "Island Duck with Mulberry Mustard",
+        };
+
+        public string[] nuts = new string[10]
+        {
+            " + almond",
+            " + cashew",
+            " + chestnut",
+            " + hazelnut",
+            " + nutmeg",
+            " + peanut",
+            " + pine nut",
+            " + pistachio",
+            " + walnut",
+            " + without any nuts",
+        };
+
+        public string[] fruits = new string[10]
+        {
+            " + apple",
+            " + avocado",
+            " + apricot",
+            " + banana",
+            " + date",
+            " + fig",
+            " + grapefruit",
+            " + grapes",
+            " + kiwi",
+            " + lemon",
+        };
+    }
+}
---
+++ Algorithms/RandomMenu/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            DishesForMenu list = new DishesForMenu();
+            InitializeComponent();
+            CreateRandomMenu(list);
+        }
+
+        private void CreateRandomMenu(DishesForMenu list)
+        {
+            Random random = new Random();
+            label1.Text = list.dishes[random.Next(0, 9)] +
+                list.nuts[random.Next(0, 9)] +
+                list.fruits[random.Next(0, 9)];
+            label2.Text = list.dishes[random.Next(0, 9)] +
+                list.nuts[random.Next(0, 9)] +
+                list.fruits[random.Next(0, 9)];
+            label3.Text = list.dishes[random.Next(0, 9)] +
+                list.nuts[random.Next(0, 9)] +
+                list.fruits[random.Next(0, 9)];
+            label4.Text = list.dishes[random.Next(0, 9)] +
+                list.nuts[random.Next(0, 9)] +
+                list.fruits[random.Next(0, 9)];
+            label5.Text = list.dishes[random.Next(0, 9)] +
+                list.nuts[random.Next(0, 9)] +
+                list.fruits[random.Next(0, 9)];
+            label6.Text = list.dishes[random.Next(0, 9)] +
+                list.nuts[random.Next(0, 9)] +
+                list.fruits[random.Next(0, 9)];
+        }
+    }
+}
