7ea66476d8479d20f42feec8cad5b1fcc90f0495
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-05-24 09:32:37 +0700

    HotUpdate

--- Algorithms/CatchAletter/Form1.Designer.cs
+++ Algorithms/CatchAletter/Form1.Designer.cs
@@ -36,8 +36,8 @@ namespace CatchAletter
             this.correctLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.missedLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.accuracyLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.totalLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.difficultyProgressBar = new System.Windows.Forms.ToolStripProgressBar();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -47,15 +47,22 @@ namespace CatchAletter
             this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 108;
+            this.listBox1.ItemHeight = 135;
             this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.MultiColumn = true;
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(856, 131);
+            this.listBox1.Size = new System.Drawing.Size(1141, 161);
             this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.correctLabel,
             this.missedLabel,
@@ -63,9 +70,10 @@ namespace CatchAletter
             this.totalLabel,
             this.toolStripStatusLabel1,
             this.difficultyProgressBar});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 109);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 135);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(856, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1141, 26);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
@@ -73,48 +81,49 @@ namespace CatchAletter
             // correctLabel
             // 
             this.correctLabel.Name = "correctLabel";
-            this.correctLabel.Size = new System.Drawing.Size(58, 17);
+            this.correctLabel.Size = new System.Drawing.Size(72, 20);
             this.correctLabel.Text = "Correct: 0";
             // 
             // missedLabel
             // 
             this.missedLabel.Name = "missedLabel";
-            this.missedLabel.Size = new System.Drawing.Size(56, 17);
+            this.missedLabel.Size = new System.Drawing.Size(70, 20);
             this.missedLabel.Text = "Missed: 0";
             // 
             // accuracyLabel
             // 
             this.accuracyLabel.Name = "accuracyLabel";
-            this.accuracyLabel.Size = new System.Drawing.Size(68, 17);
+            this.accuracyLabel.Size = new System.Drawing.Size(83, 20);
             this.accuracyLabel.Text = "Accuracy: 0";
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(482, 17);
-            this.toolStripStatusLabel1.Spring = true;
-            this.toolStripStatusLabel1.Text = "Difficulty";
-            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // totalLabel
             // 
             this.totalLabel.Name = "totalLabel";
-            this.totalLabel.Size = new System.Drawing.Size(44, 17);
+            this.totalLabel.Size = new System.Drawing.Size(58, 20);
             this.totalLabel.Text = "Total: 0";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(703, 20);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "Difficulty";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // difficultyProgressBar
             // 
             this.difficultyProgressBar.Name = "difficultyProgressBar";
-            this.difficultyProgressBar.Size = new System.Drawing.Size(100, 16);
+            this.difficultyProgressBar.Size = new System.Drawing.Size(133, 18);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(856, 131);
+            this.ClientSize = new System.Drawing.Size(1141, 161);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.listBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
--- Algorithms/CatchAletter/Form1.cs
+++ Algorithms/CatchAletter/Form1.cs
@@ -12,9 +12,21 @@ namespace CatchAletter
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count>7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("The game is over!");
+                timer1.Stop();
+            }
         }
     }
 }
