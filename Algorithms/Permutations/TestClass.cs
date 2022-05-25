3a9c45eadb59143382f97f0d9cb150372ed0d83b
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-05-26 00:14:01 +0700

    Development Update #40

---
+++ Algorithms/Permutations/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations
+{
+    internal class TestClass
+    {
+        int[] myArray = new int[5] { 1, 2, 3, 4, 5 };
+
+        private int Sum()
+        {
+            int sum = 0;
+            int i = 0;
+            while (sum<myArray.Sum())
+            {
+                sum = myArray[0] + myArray[i];
+                i++;
+                Sum();
+            }
+            return sum;
+        }
+    }
+}
