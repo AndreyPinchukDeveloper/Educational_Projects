372eacf5b026535bd64e5cf90754b3f7b1a43d8e
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-08-29 22:42:33 +0700

    This update content the following updates: - has completed UI for ListingController, all viwers we can look at main window; - created UI to edit/delete viwers ; - has created ViewModel for viwers listing.

--- YouTubeViewers/YouTubeViewersWPF/ViewModels/YouTubeViewersListingViewModel.cs
+++ YouTubeViewers/YouTubeViewersWPF/ViewModels/YouTubeViewersListingViewModel.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using YouTubeViewersWPF.ViewModels.Base;
 
 namespace YouTubeViewersWPF.ViewModels
 {
     public class YouTubeViewersListingViewModel:ViewModelBase
     {
-        public IEnumerable<YouTubeViewersListingItemViewModel> YouTubeViewersListingItemViewModels{get; { get; set; }
+        //ObservableCollection notify otside objects that collection was changed
+        //our UI automaticly update while we add or remove oblects
+        private readonly ObservableCollection<YouTubeViewersListingItemViewModel> _youTubeViewersListingItemViewModels;
+        //YouTubeViewersListingItemViewModels bind with ListView Listing controller
+        public IEnumerable<YouTubeViewersListingItemViewModel> YouTubeViewersListingItemViewModels =>
+            _youTubeViewersListingItemViewModels;
+        public YouTubeViewersListingViewModel()
+        {
+            _youTubeViewersListingItemViewModels = new ObservableCollection<YouTubeViewersListingItemViewModel>();
+            _youTubeViewersListingItemViewModels.Add(new YouTubeViewersListingItemViewModel("Inessa"));
+        }
     }
 }
