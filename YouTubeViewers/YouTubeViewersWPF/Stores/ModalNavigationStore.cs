2646cc536d991682ec983ef934b65535952a756e
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-09-06 23:09:26 +0700

    This update contain the following changes: - has created ModelNavigationStore; - has created Add/EditViewersVM.

---
+++ YouTubeViewers/YouTubeViewersWPF/Stores/ModalNavigationStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouTubeViewersWPF.ViewModels.Base;
+
+namespace YouTubeViewersWPF.Stores
+{
+    public class ModalNavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            { 
+                _currentViewModel = value;
+                CurrentViewModelChanged?.Invoke();
+            }
+        }
+        //TODO - check here(previous view model)
+        public event Action CurrentViewModelChanged;
+    }
+}
