4b9651c44aabb0a142dd035d21f9e2f63e89eeb4
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-01-27 23:04:18 +0700

    Development Update #1
    - base ViewModel, base model and VM for it.

---
+++ WPFMVVMExcusesApp/WPFMVVMExcusesApp/Models/ExcuseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVMExcusesApp.Models
+{
+    public class ExcuseModel
+    {
+        public string ExcuseName { get; }
+        public string ExcuseResult { get; }
+        public DateTime StartDate { get; }
+        public DateTime SaveToDbDate { get; }
+        public ExcuseModel(string excuseName, string excuseResult, DateTime startDate, DateTime saveToDbDate)
+        {
+            ExcuseName = excuseName;
+            ExcuseResult = excuseResult;
+            StartDate = startDate;
+            SaveToDbDate = saveToDbDate;
+        }
+    }
+}
---
+++ WPFMVVMExcusesApp/WPFMVVMExcusesApp/ViewModels/ExcuseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMVVMExcusesApp.Models;
+using WPFMVVMExcusesApp.ViewModels.Base;
+
+namespace WPFMVVMExcusesApp.ViewModels
+{
+    public class ExcuseViewModel:ViewModelBase
+    {
+        private readonly ExcuseModel _excuseModel;
+
+        public string ExcuseName => _excuseModel.ExcuseName;
+        public string ExcuseResult => _excuseModel.ExcuseResult;
+        public DateTime StartDate => _excuseModel.StartDate;
+        public DateTime SaveToDbDate => _excuseModel.SaveToDbDate;
+
+        public ExcuseViewModel(ExcuseModel excuseModel)
+        {
+            _excuseModel = excuseModel;
+        }
+    }
+}
