877614a7649e4c1b7097f076a562a3ffe811ee9c
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-02-07 22:25:49 +0700

    XAML update
    - navigationMenuButton class.

---
+++ WPFMVVMExcusesApp/FileExplorerMVVM/Infrastructure/CustomControls/NavigationMenuButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FileExplorerMVVM.Infrastructure.CustomControls
+{
+    class NavigationMenuButton:RadioButton
+    {
+
+
+        public PathGeometry Icon
+        {
+            get { return (PathGeometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(NavigationMenuButton));
+
+
+    }
+}
