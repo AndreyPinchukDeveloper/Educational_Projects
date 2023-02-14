089bad8f1a4dbe8fb90918fe218bb2d3a89ee01a
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-02-14 23:22:10 +0700

    XAML update
    - switch between dark/light mode(in progress).

---
+++ WPFMVVMExcusesApp/FileExplorerMVVM/Infrastructure/Commands/ButtonBaseCommand.cs
@@ -0,0 +1,13 @@
+﻿using FileExplorerMVVM.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorerMVVM.Infrastructure.Commands
+{
+    class ButtonBaseCommand:BaseCommand
+    {
+    }
+}
---
+++ WPFMVVMExcusesApp/FileExplorerMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FileExplorerMVVM.Infrastructure.Commands;
+using FileExplorerMVVM.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FileExplorerMVVM.ViewModels
+{
+    class MainWindowViewModel:ViewModelBase
+    {
+        ICommand ButtonBaseCommand { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            ButtonBaseCommand = new ButtonBaseCommand();
+        }
+    }
+}
