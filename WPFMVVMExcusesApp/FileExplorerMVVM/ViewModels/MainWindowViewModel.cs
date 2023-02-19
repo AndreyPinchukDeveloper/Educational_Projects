23de9c37ab428dcab215ba20a6b207762d3edd4f
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-02-19 13:12:33 +0700

    MVVM part update #1
    - base VM, models, created main fields and properties.

--- WPFMVVMExcusesApp/FileExplorerMVVM/ViewModels/MainWindowViewModel.cs
+++ WPFMVVMExcusesApp/FileExplorerMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,34 @@
 ﻿using FileExplorerMVVM.Infrastructure.Commands;
+using FileExplorerMVVM.Models;
 using FileExplorerMVVM.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace FileExplorerMVVM.ViewModels
 {
     class MainWindowViewModel:ViewModelBase
     {
+        readonly ResourceDictionary _iconDictionary = Application.LoadComponent(
+            new Uri("/FileExplorerRedesign;component/Resources/Icons.xaml", UriKind.RelativeOrAbsolute)) as ResourceDictionary;
+
+        public string ParentDirectory { get; set; }
+        public string PreviousDirectory { get; set; }
+        public string CurrentDirectory { get; set; }
+        public string NextDirectory { get; set; }
+        public string SelectedDriveSize { get; set; }
+        public string SelectedFolderDetails { get; set; }
+
+        public ObservableCollection<FileDetailsModel> FavoriteFolders { get; set; }
+        public ObservableCollection<FileDetailsModel> RemoteFolders { get; set; }
+        public ObservableCollection<FileDetailsModel> LibraryFolders { get; set; }
+        public ObservableCollection<FileDetailsModel> ConnectedDevices { get; set; }
+        public ObservableCollection<FileDetailsModel> NavigatedFolderFiles { get; set; }
         ICommand ButtonBaseCommand { get; set; }
 
 
