7c3d7b56d6cec0e0cd0f53fef56059df4c94ba9c
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-03-25 21:50:46 +0700

    UI update
    - created Property Dialog Window;
    - costom controllers for it.

---
+++ WPFMVVMExcusesApp/FileExplorerMVVM/Infrastructure/CustomControls/PropertyDialogWindow.cs
@@ -0,0 +1,34 @@
+﻿using FileExplorerMVVM.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace FileExplorerMVVM.Infrastructure.CustomControls
+{
+    public class PropertyDialogWindow:Window
+    {
+        public PathGeometry Icon
+        {
+            get { return (PathGeometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(PropertiesDialog));
+
+        public PathGeometry FileName
+        {
+            get { return (PathGeometry)GetValue(FileNameProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(PropertiesDialog));
+    }
+}
