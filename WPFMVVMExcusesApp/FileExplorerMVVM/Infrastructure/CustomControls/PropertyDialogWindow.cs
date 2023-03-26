d69620b5fb59036d2a3814cfaa4d538207b1178d
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-03-26 22:51:36 +0700

    small DP update
    - added some new dependency properties to PropertyDialogWindow and connected it with rhe same window.

--- WPFMVVMExcusesApp/FileExplorerMVVM/Infrastructure/CustomControls/PropertyDialogWindow.cs
+++ WPFMVVMExcusesApp/FileExplorerMVVM/Infrastructure/CustomControls/PropertyDialogWindow.cs
@@ -30,5 +30,87 @@ namespace FileExplorerMVVM.Infrastructure.CustomControls
         // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty FileNameProperty =
             DependencyProperty.Register("FileName", typeof(string), typeof(PropertiesDialog));
+
+        public string FileExtension
+        {
+            get { return (string)GetValue(FileExtensionProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileExtensionProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(PropertiesDialog));
+
+        public string FullPath
+        {
+            get { return (string)GetValue(FullPathProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FullPathProperty =
+            DependencyProperty.Register("FullPath", typeof(string), typeof(PropertiesDialog));
+
+        public string ModifiedOn
+        {
+            get { return (string)GetValue(ModifiedOnProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ModifiedOnProperty =
+            DependencyProperty.Register("ModifiedOn", typeof(string), typeof(PropertiesDialog));
+
+
+        public string FileSize
+        {
+            get { return (string)GetValue(FileSizeProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileSizeProperty =
+            DependencyProperty.Register("FileSize", typeof(string), typeof(PropertiesDialog));
+
+        public string CreatedOn
+        {
+            get { return (string)GetValue(CreatedOnProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CreatedOnProperty =
+            DependencyProperty.Register("CreatedOn", typeof(string), typeof(PropertiesDialog));
+
+        public string AccessedOn
+        {
+            get { return (string)GetValue(AccessedOnProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AccessedOnProperty =
+            DependencyProperty.Register("AccessedOn", typeof(string), typeof(PropertiesDialog));
+
+        public bool IsReadonly
+        {
+            get { return (bool)GetValue(IsReadonlyProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsReadonlyProperty =
+            DependencyProperty.Register("IsReadonly", typeof(bool), typeof(PropertiesDialog));
+
+        public bool IsHidden
+        {
+            get { return (bool)GetValue(IsHiddenProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsHiddenProperty =
+            DependencyProperty.Register("IsHidden", typeof(bool), typeof(PropertiesDialog));
+
     }
 }
