7970458c2dc4dd7c57abbf88a1fa9ec54730b0ba
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-04-01 22:41:18 +0700

    new console project
    - events and delegates funcionality and features.

---
+++ WPFMVVMExcusesApp/DelegatesAndEvents/Infrastructure/VideoEncoder.cs
@@ -0,0 +1,18 @@
+﻿using DelegatesAndEvents.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents.Infrastructure
+{
+    public class VideoEncoder
+    {
+        public void Encode(VideoModel video)
+        {
+            Console.WriteLine("Encoding video...");
+            Thread.Sleep(2000);
+        }
+    }
+}
