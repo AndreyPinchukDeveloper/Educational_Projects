f75d27fe7076bd93e1193dc3b61acc9eaaf6a57b
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-04-06 21:20:03 +0700

    Upload/refactoring update
    - separate Index.Razor's code to 3 methods;
    - created model to set first, last name for file etc.

---
+++ WPFMVVMExcusesApp/UploadFilesApp/Data/CustomerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UploadFilesApp.Data
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = "Ander";
+        public string? FirstName { get; set; }//nullable
+        public string? LastName { get; set; }//nullable
+        public string? FileName { get; set; }//nullable
+    }
+}
