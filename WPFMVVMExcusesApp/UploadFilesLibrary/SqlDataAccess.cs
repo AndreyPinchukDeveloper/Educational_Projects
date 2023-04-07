88ba645270e0df4fd8fb1d31ad3af8b59e0ce665
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-04-07 22:53:13 +0700

    dev Update
    - added new Library(SqlDataAccess);
    - connected all NuGetPackages;
    -added saveData method for class library.

---
+++ WPFMVVMExcusesApp/UploadFilesLibrary/SqlDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace UploadFilesLibrary;
+
+public class SqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+	{
+        _config = config;
+    }
+
+    public async Task SaveData(
+        string storedProc, 
+        string connectionName, 
+        object parameter)
+    {
+        string connectionString = _config.GetConnectionString(connectionName)
+            ?? throw new Exception($"Missing connection string at {connectionName}");
+
+        using var connection = new SqlConnection(connectionString);
+
+        await connection.ExecuteAsync(
+            storedProc, 
+            parameter, 
+            commandType: System.Data.CommandType.StoredProcedure);
+    }
+}
