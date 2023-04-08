804b51ee96d1371ca54ebb2700fbc399e576d637
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-04-09 01:21:38 +0700

    Test Update
    - test.

--- WPFMVVMExcusesApp/UploadFilesApp/Program.cs
+++ WPFMVVMExcusesApp/UploadFilesApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using UploadFilesApp.Data;
+using UploadFilesApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
