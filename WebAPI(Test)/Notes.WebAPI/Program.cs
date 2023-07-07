3dc22799fa9f4941e401587798faee51c4368990
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-07-07 23:31:33 +0700

    dev Update
    - settings for Program.cs file.

--- WebAPI(Test)/Notes.WebAPI/Program.cs
+++ WebAPI(Test)/Notes.WebAPI/Program.cs
@@ -1,5 +1,31 @@
+using Microsoft.Extensions.Hosting;
+using Notes.Application.Common.Mapping;
+using Notes.Application.Interfaces;
+using Notes.Persistance;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+using(var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+	try
+	{
+		var context = serviceProvider.GetRequiredService<NotesDbContext>();
+		DbInitializer.Initialize(context);
+	}
+	catch (Exception)
+	{
+
+		throw;
+	}
+}
+builder.Services.AddAutoMapper(config =>
+{
+	config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+	config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
 
 app.MapGet("/", () => "Hello World!");
 
