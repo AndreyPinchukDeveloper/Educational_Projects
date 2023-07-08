839a5ff5876075bbf741f8688ef9ba02e1b51ceb
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-07-08 21:21:13 +0700

    dev Update
    - settings for Program.cs;
    - added base controller class.

--- WebAPI(Test)/Notes.WebAPI/Program.cs
+++ WebAPI(Test)/Notes.WebAPI/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.Extensions.Hosting;
+using Notes.Application;
 using Notes.Application.Common.Mapping;
 using Notes.Application.Interfaces;
 using Notes.Persistance;
 using System.Reflection;
+
+public IConfiguration Configuration { get; }
+
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -21,11 +25,34 @@ using(var scope = app.Services.CreateScope())
 		throw;
 	}
 }
+
 builder.Services.AddAutoMapper(config =>
 {
 	config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
 	config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
 });
+builder.Services.AddApplication();
+builder.Services.AddPersistance();//!!!!!
+
+builder.Services.AddCors(options =>//only for tests !!!!
+{
+	options.AddPolicy("AllowAll", policy =>
+	{
+		policy.AllowAnyHeader();
+		policy.AllowAnyMethod();
+		policy.AllowAnyOrigin();
+	});
+});
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");//only for tests !!!!
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllers();
+});
+
 
 app.MapGet("/", () => "Hello World!");
 
