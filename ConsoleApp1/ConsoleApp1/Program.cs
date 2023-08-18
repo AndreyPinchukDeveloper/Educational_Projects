769d37dd02163b403170fec3e4bd7a845e6fbf42
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-08-18 23:36:01 +0700

    Demo web api
    -base settings (.net 6)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+
+void Sum(out int a)
+{
+    a++;
+}
+
+void DoSomething(List<int> list)
+{
+    for (int i = 0; i < list.Count; i++)
+    {
+        Sum(list[i]);
+    }
+}
+
+
+DoSomething(list);
+
---
+++ PythonConsoleTestApp/PythonConsoleTestApp/PythonConsoleTestApp.py
@@ -0,0 +1,77 @@
+# Включить поддержку Python и загрузить библиотеку DesignScript
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+clr.AddReference('RevitServices') 
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+import itertools
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+def get_fam(a):
+	elements = FilteredElementCollector(doc).OfCategory(a).WhereElementIsNotElementType().ToElements()
+	return elements	
+	
+def write_par (a,b):
+	error_list = []
+	for element in a:
+		try:
+			name = element.LookupParameter(b).AsValueString()
+			level = element.LookupParameter('Этаж №')
+			if name:
+				level.Set(name)
+
+		except AttributeError:
+			error_list.append(element)
+		except:
+			error_list.append(element)			
+	return error_list
+
+parts = get_fam(BuiltInCategory.OST_Parts) #Базовый уровень
+stairs = get_fam(BuiltInCategory.OST_Stairs) #Базовый уровень 
+struct_fram = get_fam(BuiltInCategory.OST_StructuralFraming) #Базовый уровень 
+railing = get_fam(BuiltInCategory.OST_StairsRailing) #Базовый уровень 
+roofs = get_fam(BuiltInCategory.OST_Roofs) #Базовый уровень
+
+walls = get_fam(BuiltInCategory.OST_Walls) #Зависимость снизу
+struct_col = get_fam(BuiltInCategory.OST_StructuralColumns) #Зависимость снизу
+
+floors = get_fam(BuiltInCategory.OST_Floors) #Уровень 
+generic = get_fam(BuiltInCategory.OST_GenericModel) #Уровень 
+windows = get_fam(BuiltInCategory.OST_Windows) #Уровень
+doors = get_fam(BuiltInCategory.OST_Doors) #Уровень
+foundation = get_fam(BuiltInCategory.OST_StructuralFoundation) #Уровень
+rooms = get_fam(BuiltInCategory.OST_Rooms) #Уровень
+furniture = get_fam(BuiltInCategory.OST_Furniture) #Уровень
+equipment = get_fam(BuiltInCategory.OST_MechanicalEquipment) #Уровень
+plumbingfixtures = get_fam(BuiltInCategory.OST_PlumbingFixtures) #Уровень
+specialEquipment = get_fam(BuiltInCategory.OST_SpecialityEquipment) #Уровень
+
+elements_base = list(itertools.chain(parts,stairs,railing,roofs,struct_fram))
+elements_niz = list(itertools.chain(walls,struct_col))
+elements_level = list(itertools.chain(floors,generic,windows,doors,foundation,rooms,furniture,equipment,plumbingfixtures,specialEquipment))
+
+all = list(itertools.chain(elements_base,elements_niz,elements_level)) 
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+base = write_par(elements_base,'Базовый уровень')
+niz = write_par(elements_niz,'Зависимость снизу')
+level = write_par(elements_level,'Уровень')
+
+TransactionManager.Instance.TransactionTaskDone()
+
+
+OUT = niz, level, base
+
+#assemblies = get_fam(BuiltInCategory.OST_Assemblies)
+
+
+
+
+
+
