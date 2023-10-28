4fbf9df57869e487f7c7b0ddfc1ff9e46556b63a
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-10-28 15:13:03 +0700

    dev update
    - html parser template has beeen done.

---
+++ API/DataParserAPI/DataParser2/Models/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataParser2.Models
+{
+    public class Card
+    {
+        public string Price { get; set; }
+        public string Title { get; set; }
+
+        public void Parse(string html)
+        {
+            var priceStart = html.IndexOf("Price") + 11;
+            var priceEnd = html.IndexOf("<span", priceStart);
+            Price = html.Substring(priceStart, priceEnd - priceStart).Trim();
+
+            var titleStart = html.IndexOf("<h1>") + 4;
+            var titleEnd = html.IndexOf("</h1>", titleStart);
+            Title = html.Substring(titleStart, titleEnd - titleStart).Trim();
+        }
+    }
+}
--- API/DataParserAPI/DataParser2/Program.cs
+++ API/DataParserAPI/DataParser2/Program.cs
@@ -1,4 +1,5 @@
-﻿using SupportLibrary;
+﻿using DataParser2.Models;
+using SupportLibrary;
 using System.Net;
 
 var code = "416001653";
@@ -30,4 +31,26 @@ var stringEnd = postRequest.Response.IndexOf("\"", stringStart);
 var getPath = postRequest.Response.Substring(stringStart, stringEnd - stringStart);
 
 Console.WriteLine($"getPath={getPath}");
+
+var getRequest = new GetRequest($"https://baucenter.ru{getPath}");
+getRequest.Accept = "";
+getRequest.UserAgent = "";
+getRequest.Referer = "";
+
+getRequest.Headers.Add("", "");
+getRequest.Headers.Add("", "");
+getRequest.Headers.Add("", "");
+getRequest.Headers.Add("", "");
+getRequest.Headers.Add("", "");
+getRequest.Headers.Add("", "");
+getRequest.Headers.Add("", "");
+getRequest.Host = "baucenter.ru";
+getRequest.Proxy = proxy; 
+getRequest.Run(cookieContainer);
+
+var card = new Card();
+card.Parse(getRequest.Response);
+Console.WriteLine($"title={card.Title}");
+Console.WriteLine($"price={card.Title}");
+
 Console.ReadLine();
