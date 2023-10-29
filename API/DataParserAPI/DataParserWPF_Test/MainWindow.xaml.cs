0707ae2e7cddd44ff7aa0f511c6f7b3da61cabb1
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-10-29 13:39:30 +0700

    dev Update
    - created View part for parser.

---
+++ API/DataParserAPI/DataParserWPF_Test/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataParserWPF_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {          
+            if (DateFrom.SelectedDate == null || DateTo.SelectedDate == null || string.IsNullOrEmpty(Article.Text))
+            {
+                MessageBox.Show("How about date or maybe you forget news registration number ?");
+                return;
+            }
+            var dateFrom = (DateTime)DateFrom.SelectedDate;
+            var dateTo = (DateTime)DateFrom.SelectedDate;
+            var artickle = Article.Text;
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"https://github.com/AndreyPinchukDeveloper";
+        }
+    }
+}
