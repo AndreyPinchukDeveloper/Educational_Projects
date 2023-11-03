f19d4e9c9d11c09997ca87a764f8bc2494a76f6d
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-11-03 20:14:38 +0700

    dec update
    - FV practice.

--- API/DataParserAPI/Cooker/FluentValidation/TestClass.cs
+++ API/DataParserAPI/Cooker/FluentValidation/TestClass.cs
@@ -34,13 +34,22 @@ namespace Cooker.FluentValidation
     {
         public EntityValidator()
         {
-            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().When(x => !x.Admin);
             RuleFor(x => x.Description).NotEmpty().MaximumLength(50);
-            RuleFor(x => x.Logins).Must(x => x != null && x.Count > 0).WithMessage("There's not any logins");
+            RuleFor(x => x.Logins).Must(x => x != null && x.Count > 0).OverridePropertyName("NewPropertyName").WithMessage("There's not any logins");
             RuleForEach(x => x.Logins).SetValidator(new LoginValidator());
+            RuleForEach(x => x.Logins)
+                .ChildRules(rule => rule.RuleFor(x => x.Email).SetValidator(""));
+
+
+            RuleSet("RuleName", () =>
+            {
+                RuleFor(x => x.Name).NotEmpty();
+                RuleFor(x => x.Description).MinimumLength(50);
+            });
         }
     }
-
+     
     public class LoginValidator : AbstractValidator<Login>
     {
         public LoginValidator()
@@ -49,11 +58,24 @@ namespace Cooker.FluentValidation
             RuleFor(x => x.NickName).NotEmpty().MaximumLength(50);          
         }
     }
+    public class Login2Validator : AbstractValidator<Login2>
+    {
+        public Login2Validator()
+        {
+            RuleFor(x => x.Email2).NotEmpty();
+            RuleFor(x => x.NickName2).NotEmpty().MaximumLength(50);
+        }
+    }
 
     public class Login
     {
         public string Email { get; set; }
         public string NickName { get; set; }
+    }
+    public class Login2
+    {
+        public string Email2 { get; set; }
+        public string NickName2 { get; set; }
     }
 
     public class Entity
@@ -63,5 +85,7 @@ namespace Cooker.FluentValidation
         public int IntValue { get; set; }
 
         public List<Login> Logins { get; set; }
+        public List<Login2> Logins2 { get; set; }
+        public bool Admin { get; internal set; }
     }
 }
