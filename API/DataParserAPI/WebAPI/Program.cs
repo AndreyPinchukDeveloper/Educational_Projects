9a4a678e2bea40b61e84644cf631477f1974550b
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-11-12 20:07:10 +0700

    dev update
    - practice with health checks API.

--- API/DataParserAPI/WebAPI/Program.cs
+++ API/DataParserAPI/WebAPI/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
 builder.Services.AddTransient<IStaffRepository, StaffRepository>();
 builder.Services.AddScoped<IScopedService, TestService>();
 builder.Services.AddTransient<ITransientService, TestService>();
@@ -28,30 +29,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
+app.UseEndpoints(endpoints =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    endpoints.MapControllers();
+    //endpoints.MapHealthChecks("I'm healthy");
+    endpoints.MapHealthChecksUI();
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();//that method always must be at the end  
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();
