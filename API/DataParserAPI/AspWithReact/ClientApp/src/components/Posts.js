235b70792f1533f72ed7e190f3ede034c3febc7b
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-11-25 19:34:19 +0700

    dev update
    - there was expanded Posts.js.

--- API/DataParserAPI/AspWithReact/ClientApp/src/components/Posts.js
+++ API/DataParserAPI/AspWithReact/ClientApp/src/components/Posts.js
@@ -1,30 +1,80 @@
+import { useEffect, useState } from "react";
+
+const URL = `/api/posts`;
+
 const Posts = () => {
 
     const [allPosts, setPosts] = useState([]);
 
     const getPosts = async () => {
         const options = {
-            method: 'GET'
+            method: 'GET',
+            headers: new Headers()
         }
-        const result = await fetch('https://localhost:7131', options);
+        const result = await fetch(URL, options);
         if (result.ok) {
             const posts = await result.json();
+            setPosts(posts);
             return posts;
         }
+        return [];
     }
+
+    const addPost = async () => {
+
+        const headerFromUser = document.querySelector('#header').value;
+        const textFromUser = document.querySelector('#text').value;
+
+        const newPost = {
+            header: headerFromUser,
+            text: textFromUser
+        };
+
+        const options = {
+            method: 'POST',
+            headers: new Headers(),
+            body: JSON.stringify(newPost)
+        };
+
+        const result = await fetch(URL, options);
+        if (result.ok) {
+            const post = await result.json();
+            setPosts(allPosts.push(post));
+        }
+        return [];
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
 
     return (
         <div>
-            <p>Posts creator</p>
-            <div style={{ margin: '10px' }}>
-                <input type="text" />
+            <div>
+                <p>Posts creator</p>
+                <div style={{ margin: '10px' }}>
+                    <input id="header" type="text" />
+                </div>
+                <div style={{ margin: '10px' }}>
+                    <textarea id="text" />
+                </div>
+                <button onClick={() => addPost()}>Add post</button>
             </div>
-            <div style={{ margin: '10px' }}>
-                <textarea />
+            <div>
+                {allPosts.map(x => <PostItem key={x.id} id={x.id} header={x.header} text={x.text}/>})}
             </div>
-            <button>Add post</button>
         </div>
+        
     )
 }
 
 export default Posts;
+
+const PostItem = ({ id, header, text }) => {
+    return (
+        <div>
+            <h2>{x.header}</h2>
+            <p>{x.text}</p>
+        </div>
+    )
+}
