d01a43ba1e80726695dcc6245cbcb3636c776cce
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-12-24 22:22:51 +0700

    dev update
    - test controller method.

--- API/DataParserAPI/HangfireAPI/Controllers/HangfireController.cs
+++ API/DataParserAPI/HangfireAPI/Controllers/HangfireController.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HangfireAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HangfireController:ControllerBase 
     {
-        [HttpGet]
-        public IActionResult Get()
+        [HttpPost]
+        [Route("action")]
+        public IActionResult Welcome()
         {
-            return Ok("Hello from HanfgireAPI !");
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome? stranher !"));
+            return Ok($"Job ID: {jobId}. Welcome email sent to user !");
+        }
+
+        private void SendWelcomeEmail(string text)
+        {
+            Console.WriteLine(text);
         }
     }
 }
