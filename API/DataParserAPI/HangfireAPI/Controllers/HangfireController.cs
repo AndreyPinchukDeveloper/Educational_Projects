5252758c4f341466229bdc2fbbcfc2a4a6fd2b7b
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-12-25 13:43:46 +0700

    dev update
    hangfire practice

--- API/DataParserAPI/HangfireAPI/Controllers/HangfireController.cs
+++ API/DataParserAPI/HangfireAPI/Controllers/HangfireController.cs
@@ -15,6 +15,33 @@ namespace HangfireAPI.Controllers
             return Ok($"Job ID: {jobId}. Welcome email sent to user !");
         }
 
+        [HttpPost]
+        [Route("action")]
+        public IActionResult Discount()
+        {
+            int timeInSeconds = 30;
+            var jobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Welcome? stranher !"), TimeSpan.FromSeconds(timeInSeconds));
+            return Ok($"Job ID: {jobId}. Discount email will be sent in {timeInSeconds} seconds !");
+        }
+
+        [HttpPost]
+        [Route("action")]
+        public IActionResult DatabaseUpdate()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Database was updated"), Cron.Minutely);
+            return Ok("Database check job was initiated !");
+        }
+
+        [HttpPost]
+        [Route("action")]
+        public IActionResult Confirm()
+        {
+            int timeInSeconds = 30;
+            var parentJobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Unsubscribe"), TimeSpan.FromSeconds(timeInSeconds));
+            BackgroundJob.ContinueJobWith(parentJobId, () => Console.WriteLine("You were unsubscribed :("));
+            return Ok($"Confirmation job created !");
+        }
+
         private void SendWelcomeEmail(string text)
         {
             Console.WriteLine(text);
