07ee8f9f1d20f88fb0763e56153525a17320a695
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-12-26 07:36:14 +0700

    dev Update
    - added 2 class library an base models.

---
+++ API/DataParserAPI/SchoolHRAdministration/Models/DeputyHeadMaster.cs
@@ -0,0 +1,13 @@
+﻿using HRAdministrationAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolHRAdministration.Models
+{
+    public class DeputyHeadMaster:EmployeeBase
+    {
+    }
+}
---
+++ API/DataParserAPI/SchoolHRAdministration/Services/ManagerService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRAdministrationAPI;
+using SchoolHRAdministration.Models;
+
+namespace SchoolHRAdministration.Services
+{
+    public class ManagerService
+    {
+        public void SeedData(List<IEmployee> employees)
+        {
+            IEmployee teacher1 = new Teacher()
+            {
+                Id = 1,
+                FirstName = "Andre",
+                LastName = "Great",
+                Salary = 50000
+            };
+            employees.Add(teacher1);
+
+            IEmployee teacher2 = new Teacher()
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Great",
+                Salary = 50000
+            };
+            employees.Add(teacher2);
+
+            IEmployee headDepartment = new HeadOfDepartment()
+            {
+                Id = 3,
+                FirstName = "Bran",
+                LastName = "Great",
+                Salary = 100000
+            };
+            employees.Add(headDepartment);
+
+            IEmployee deputyHeadMaster = new DeputyHeadMaster()
+            {
+                Id = 4,
+                FirstName = "Rohn",
+                LastName = "Great",
+                Salary = 80000
+            };
+            employees.Add(deputyHeadMaster);
+
+            IEmployee headMaster = new HeadMaster()
+            {
+                Id = 5,
+                FirstName = "Helga",
+                LastName = "Great",
+                Salary = 75000
+            };
+            employees.Add(headMaster);
+        }
+    }
+}
