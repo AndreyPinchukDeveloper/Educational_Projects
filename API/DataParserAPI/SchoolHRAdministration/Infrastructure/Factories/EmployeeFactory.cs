2d48ae100eee6cd8b903c7cc14ebfeda2e456186
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2024-01-02 20:31:54 +0700

    dev update
    - created factory for all school department.

---
+++ API/DataParserAPI/SchoolHRAdministration/Infrastructure/Factories/EmployeeFactory.cs
@@ -0,0 +1,37 @@
+﻿using HRAdministrationAPI;
+using SchoolHRAdministration.Enums;
+using SchoolHRAdministration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolHRAdministration.Infrastructure.Factories
+{
+    public class EmployeeFactory
+    {
+        public static IEmployee GetEmployeeInstance(EmployeeType employeeType, int id, string firstName, string lastName, decimal salary)
+        {
+            IEmployee employee = null;
+            switch (employeeType) 
+            {
+                case EmployeeType.Teacher:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.HeadOfDepartment:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.DeputyHeadMaster:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.HeadMaster:
+                    employee = new Teacher { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                default:
+                    break;
+            }
+            return employee;
+        }
+    }
+}
