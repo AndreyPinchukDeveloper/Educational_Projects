0804d1aa13a333419a33495754e92a35bf9e669f
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2024-01-13 22:56:10 +0700

    dev update
    - settings for validation class.

--- API/DataParserAPI/FieldValidatorAPI/CommonFieldValidatorFunctions.cs
+++ API/DataParserAPI/FieldValidatorAPI/CommonFieldValidatorFunctions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FieldValidatorAPI
@@ -20,9 +21,85 @@ namespace FieldValidatorAPI
         private static PatternMatchValidDelegate _patternMatchValidDelegate = null;
         private static CompareFieldsValidDelegate _compareFieldsValidDelegate = null;
 
-        private static bool RequiredValidDelegate(string fieldValue)
+        /// <summary>
+        /// Singleton pattern. Only one delegate can be exist ! 
+        /// </summary>
+        #region Delegates' Properties
+        public static CompareFieldsValidDelegate CompareFieldsValidDelegate
+        {
+            get
+            {
+                if (_compareFieldsValidDelegate == null)
+                    _compareFieldsValidDelegate = new CompareFieldsValidDelegate(CompareFieldsValidDelegate);
+                return _compareFieldsValidDelegate;
+            }
+        }
+        public static PatternMatchValidDelegate PatternMatchValidDelegate
+        {
+            get
+            {
+                if (_patternMatchValidDelegate == null)
+                    _patternMatchValidDelegate = new PatternMatchValidDelegate(PatternMatchValidDelegate);
+                return _patternMatchValidDelegate;
+            }
+        }
+        public static DateValidDelegate DateValidDelegate
+        {
+            get
+            {
+                if (_dateValidDelegate == null)
+                    _dateValidDelegate = new DateValidDelegate(DateValidDelegate);
+                return _dateValidDelegate;
+            }
+        }
+        public static RequiredValidDelegate RequiredValidDelegate
+        {
+            get
+            {
+                if (_requiredValidDelegate == null) 
+                    _requiredValidDelegate = new RequiredValidDelegate(RequiredValidDelegate);
+                return _requiredValidDelegate;
+            }
+        }
+
+        public static StringLengthValidDelegate StringLengthValidDelegate
+        {
+            get
+            {
+                if (_stringLengthValidDelegate == null)
+                    _stringLengthValidDelegate = new StringLengthValidDelegate(StringLengthValidDelegate);
+                return _stringLengthValidDelegate;
+            }
+        }
+        #endregion
+
+
+        #region Methods
+        private static bool RequiredFieldValid(string fieldValue)
         {
             return !String.IsNullOrWhiteSpace(fieldValue) ? true : false;
         }
+
+        private static bool RequiredFieldLengthValid(string fieldValue, int min, int max)
+        {
+            return fieldValue.Length >= min && fieldValue.Length<= max ? true : false;
+        }
+
+        private static bool DateFieldValid(string dateTime, out DateTime validDateTime)
+        {
+            return DateTime.TryParse(dateTime, out validDateTime) ? true : false;
+        }
+
+        private static bool FieldPatternValid(string fieldVal, string regularExpressionPattern)
+        {
+            Regex regex = new Regex(regularExpressionPattern);
+            return regex.IsMatch(fieldVal) ? true : false;
+        }
+
+        private static bool FieldComparisonValid(string field1, string field2)
+        {
+            return field1.Equals(field2) ? true : false;
+        }
+        #endregion
     }
 }
